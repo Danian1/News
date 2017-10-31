ad86a9357065684cf6d02e8096adf3f970cc848a
Țurcan Danian <turcan.danu@gmail.com>
2017-10-31 13:00:18 +0200

    Add files via upload

---
+++ storage/framework/views/4637bf0358b86976b955517e6ede10f68a76b093.php
@@ -0,0 +1,71 @@
+ 
+	
+    <!-- Page Content -->
+    <div class="container">
+		<div class="row">
+			<a href="<?php echo e(URL::route('admin')); ?>"><button type="submit" class="btn btn-primary">ADD NEWS</button></a>
+		</div>
+
+	  <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+	  <hr>
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-6">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/500x200" alt="">
+                </a>
+            </div>
+            <div class="col-md-6">
+				<h3><?php echo e($value->title); ?> </h3>
+                <h4><?php echo e($value->source); ?>, <?php echo e($value->datatime); ?></h4>
+                <p><?php echo e($value->desc); ?></p>
+				<p><a href="<?php echo e($value->link); ?>"><button class="btn btn-info">Detalii</button></a></p>
+            </div>
+        </div>
+        <!-- /.row -->
+     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <hr>
+
+        <!-- Pagination 
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">&laquo;</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">&raquo;</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        		row -->
+
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="<?php echo e(url('js/jquery.js')); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo e(url('js/bootstrap.min.js')); ?>"></script>
+
+</body>
+
+</html>
+
+<?php echo $__env->make('layout.head', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/ba1fa287d1145dd08eca9dbded8ddb76ebd788a8.php
@@ -0,0 +1,45 @@
+﻿ 
+
+<div class="container">
+<?php $__currentLoopData = $url; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $tag): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <form method="post" action="/insert">
+  <?php echo e(csrf_field()); ?>
+
+    <div class="form-group row">
+      <label for="link" class="col-sm-2 col-form-label">Link-ul</label>
+      <div class="col-md-6">
+        <input type="text" class="form-control" id="link" name="link" placeholder="Link" >
+	  </div>
+	  <div class="col-md-3">
+		<button type="genetare" class="btn btn-primary">Generate info to link</button>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="title" class="col-sm-2 col-form-label">Title</label>
+      <div class="col-md-6">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="<?php echo e($tag->title); ?>">
+      </div>
+    </div>
+	<div class="form-group row">
+      <label for="source" class="col-sm-2 col-form-label">Source</label>
+      <div class="col-md-6">
+        <input type="text" class="form-control" id="source" name="source" placeholder="Source" value="">
+      </div>
+    </div>
+	<div class="form-group row">
+      <label for="desc" class="col-sm-2 col-form-label">Description</label>
+      <div class="col-md-6">
+         <textarea class="form-control" rows="5" id="desc" name="desc" placeholder="Description"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="offset-sm-2 col-sm-10">
+        <button type="submit" class="btn btn-success" name="save"> Save</button>
+		<input type="reset" class="btn btn-info" name="reset" value="Reset"/>
+      </div>
+    </div>
+  </form>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</div>
+
+<?php echo $__env->make('layout.head', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
